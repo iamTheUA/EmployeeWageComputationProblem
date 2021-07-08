842879e1b405adcdd9d61d3c5b402cfb18b64458
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-07-08 13:35:41 +0530

    Adding: Case Statement

--- EmployeeWageComp.sh
+++ EmployeeWageComp.sh
@@ -1,7 +1,7 @@
 echo "Welcome to Employee Wage Computation Program"
 
-#if attendance = 1 then It is Present and if =0 then It is Absent
-attendance=$((RANDOM%2))
+#if attendance = 1 then It is Present and if =0 then It is Absent and =2 then it is part time
+attendance=$((RANDOM%3))
 
 #Calculating Daily Employee Wage
 WagePerHour=20
@@ -10,3 +10,17 @@ DailyWage=$((WagePerHour*FullDayHour))
 
 #Part time hour
 PartDayHour=4
+
+case "$attendance" in
+	1)
+		DailyWage=$((WagePerHour*FullDayHour))
+		;;
+	2)
+		DailyWage=$((WagePerHour*PartDayHour))
+		;;
+	0)
+		DailyWage=0
+		;;
+	*)
+esac
+
